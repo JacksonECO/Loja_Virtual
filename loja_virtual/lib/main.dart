7e9973cfb44d8fe8d1eb9848e1a8e95564268f8c
Jackson Galdino <jacksongdas.jackson@gmail.com>
2020-08-21 00:03:49 -0300

    inicia Firebase
    tem erro, na link da foto e com o Google

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -7,26 +7,28 @@ import 'package:lojavirtual/Model/user_model.dart';
 import 'package:lojavirtual/main_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-void main(){
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(Ini());
 }
 
 class Ini extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final FirebaseOptions firebaseOptions = const FirebaseOptions(
-      appId: '1:448618578101:ios:0b650370bb29e29cac3efc',
-      apiKey: 'AIzaSyAgUhHU8wSJgO5MVNy95tMT07NEjzMOfz0',
-      projectId: 'react-native-firebase-testing',
-      messagingSenderId: '448618578101',
-    );
+//    final FirebaseOptions firebaseOptions = const FirebaseOptions(
+//      apiKey: "AIzaSyAlr-89POnxazWjQq4r8bQ9qYzAENb7z8M",
+//      authDomain: "loja-virtual-f8eca.firebaseapp.com",
+//      databaseURL: "https://loja-virtual-f8eca.firebaseio.com",
+//      projectId: "loja-virtual-f8eca",
+//      storageBucket: "loja-virtual-f8eca.appspot.com",
+//      messagingSenderId: "840191066817",
+//      appId: "1:840191066817:android:272cdd806c6aaab9604a40",
+//    );
     return FutureBuilder(
-      future: Firebase.initializeApp(name: "oi", options: firebaseOptions),
+      //future: Firebase.initializeApp(),
       builder: (context, snapshot){
         print(ConnectionState.active);
-        Future.delayed(Duration(seconds: 4));
         if(snapshot.hasError){
           print("erro: ${snapshot.error}");
           return Center( child: CircularProgressIndicator());
